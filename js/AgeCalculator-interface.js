a842a284f7863038b979e482b0b0d5e8914787c2
sravanthi velaga <svelaga0412@gmail.com>
2017-11-17 16:48:04 -0800

    with js files

--- js/AgeCalculator-interface.js
+++ js/AgeCalculator-interface.js
@@ -1,9 +1,15 @@
 import {AgeCalculator} from './../js/AgeCalculator.js';
 
-
-
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $('#formOne').submit(function(event) {
     event.preventDefault();
+    let date = new Date($("#dob").val());
+    let calculator = new AgeCalculator(date);
+    $('#mercury').text(calculator.ageOnMercury());
+    $('#venus').text(calculator.ageOnVenus());
+    $('#mars').text(calculator.ageOnMars());
+    $('#jupiter').text(calculator.ageOnJupiter());
 
+  });
+});
 });
--- js/AgeCalculator.js
+++ js/AgeCalculator.js
@@ -1,34 +1,83 @@
 export class AgeCalculator {
-constructor () {
-
+constructor (dob) {
+  this.secondsInOneYear = 31536000; // 60 * 60 * 24 * 365
+  this.dob = dob; // Date object
+  this.lifeExpectancy = Math.random() * (82 - 76) + 76;  //life expectancy between the ages of 76 and 81 in earth years
 }
 
-ConvertAgeToSeconds(age) {
-    //Converting given ageInput to seconds
-    let year = 31,557,600;
-    let yearToSeconds = age*year;
+convertAgeToSeconds() {
+    //Converting given bodInput to seconds
 
-    return yearToSeconds;
-  }
+  let currentDate = new Date();
+  return yearToSeconds = parseInt(currentDate.setTime(this.dob.getTime())/1000); //divide by 1000 as the getTime returns milliseconds
+}
 
-  CheckSyllables() {
-    //Checking if Line 1 has 5 syllables, line 2 has 7 and line 3 has 5 syllables
+ageInYears() {
+  //converting age in yearToSeconds
 
-    var syllable = require('syllable');
-    let IsHaiku = false;
-    let lineOne = this.lineOne;
-    let lineTwo = this.lineTwo;
-    let lineThree = this.lineThree;
-    let lineOneSyllable = syllable(lineOne);
-    let lineTwoSyllable = syllable(lineTwo);
-    let lineThreeSyllable = syllable(lineThree);
+  let age = parseInt(this.ConvertAgeToSeconds()/secondsInOneYear);
+  return age;
+}
+
+lifeExpOnEarth(){
+  //caliculating life expectancy on earth
+
+  let lifeExpOnEarth = this.lifeExpectancy - this.ageInYears();
+  return lifeExpOnEarth;
+}
+
+ageOnMercury(){
+  let mercuryAge = parseInt(this.ageInYears()/0.24);
+  return mercuryAge;
+}
+ageOnVenus(){
+  let venusAge = parseInt(this.ageInYears()/0.62);
+  return venusAge;
+}
+ageOnMars(){
+  let marsAge = parseInt(this.ageInYears()/1.88);
+  return marsAge;
+}
+ageOnJupiter(){
+  let jupiterAge = parseInt(this.ageInYears()/11.86);
+  return jupiterAge;
+}
 
 
-    if( lineOneSyllable == 5 && lineTwoSyllable == 7 && lineThreeSyllable == 5)
-    {
-      IsHaiku = true;
+
+lifeExpectancyOnMercury(){
+  let lifeExpOnMercury = parseInt(this.lifeExpOnEarth()/.24);
+  return lifeExpOnMercury;
+}
+lifeExpectancyOnVenus(){
+  let lifeExpOnVenus = parseInt(this.lifeExpOnEarth()/.62);
+  return lifeExpOnVenus;
+}
+lifeExpectancyOnMars(){
+  let lifeExpOnMars = parseInt(this.lifeExpOnEarth()/1.88);
+  return lifeExpOnMars;
+}
+lifeExpectancyOnVenus(){
+  let lifeExpOnJupiter = parseInt(this.lifeExpOnEarth()/11.86);
+  return lifeExpOnJupiter;
+}
+
+// lifeOverExpectancy(){
+//   let lifeOverExpectancy = this.ageInYears - this.lifeExpOnEarth;
+//   return lifeOverExpectancy;
+// }
+
+
+isAlive(){
+  if(this.ageInYears() >= this.lifeExpectancy) {
+      return true;
     }
-    return IsHaiku;
-  }
+    return false;
+}
+
+
+
+
+
 
 }
